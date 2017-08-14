40d533541bb721de0ed4ec1dd19ed38bceead737
412115725@qq.com <3qsqec2a5v>
2017-08-14 20:49:29 +0800

    Initial commit

---
+++ app/src/main/java/bomb/dec/com/haha/MainActivity.java
@@ -0,0 +1,13 @@
+package bomb.dec.com.haha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
